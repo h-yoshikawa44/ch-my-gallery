ded07a4bb6f0764f627f2672bc4bf0595d2c929c
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-12-03 17:08:32 +0900

    settings: Viteのbaseから末尾スラッシュが抜けていたので追記(#7)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/ch-my-gallery' : './',
+  base: process.env.NODE_ENV === 'production' ? '/ch-my-gallery/' : './',
   server: {
     open: true,
   },
